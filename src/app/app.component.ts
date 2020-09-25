901051f5b3a9ff6cc5c1c689d81189dd062b4a11
jarvis <jarvisrizo_southsystem@getnet.br>
2020-09-25 00:37:22 -0300

    configurando keycloak login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthConfig, NullValidationHandler, OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iscob-ng';
+
+  constructor(private oauthService: OAuthService){
+	  this.configure();
+  }
+
+  authConfig: AuthConfig = {
+    issuer: 'http://localhost:8080/auth/realms/iscob',
+    redirectUri: window.location.origin,
+    clientId: 'iscob-frontend',
+    responseType: 'code',
+    scope: 'openid profile email offline_access',
+    showDebugInformation: true,
+  };
+
+  configure(): void{
+	this.oauthService.configure(this.authConfig);
+	this.oauthService.tokenValidationHandler = new NullValidationHandler();
+	this.oauthService.setupAutomaticSilentRefresh();
+	this.oauthService.loadDiscoveryDocument().then(()=> this.oauthService.tryLogin());
+  }
+
+  login():void {
+	this.oauthService.initImplicitFlowInternal();
+  }
+
+  logout():void {
+	this.oauthService.logOut()
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+	AppRoutingModule,
+	HttpClientModule,
+	FormsModule,
+	OAuthModule.forRoot({
+		resourceServer: {
+			allowedUrls: ['http://localhost:8081/test'],
+			sendAccessToken: true
+		}
+	})
   ],
   providers: [],
   bootstrap: [AppComponent]
